4d183bbe73156d6e4444e4dee1e56731f036c0b0
j721 <juliadvu@gmail.com>
2020-02-27 14:54:11 -0600

    Task 1 completed and task 2 and 3 are being worked on

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
-
+    constructor(name, age, stomach){
+      this.name = name; 
+      this.age = age;
+      this.stomach = [];
+    }
+  eat(someFood){
+    if(this.stomach.length <10){
+        this.stomach.push(someFood);
+    }
+  }
+  poop(){
+    return this.stomach.length = 0; 
+  }
+  toString(){
+    return `${this.name} ${this.age}`;
+  }
 }
 
 /*
@@ -59,7 +74,19 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0; 
+  }
+  fill(gallons){
+    this.tank = this.tank = gallons; 
+  }
+  drive(distance){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank 
+  }
 }
 
 /*
@@ -75,8 +102,21 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+    constructor(person){
+        this.name = person.name;
+        this.age = person.age;
+        this.location = person.location;
+    }
+    speak(){
+      return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
 }
+
+const person = new Lambdasian({
+  name: 'Joe',
+  age: 20,
+  location: 'Usa'
+})
 
 /*
   TASK 4
